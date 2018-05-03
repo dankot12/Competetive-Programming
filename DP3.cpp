e6363d734fe4ee74c24380eb8786e3e0ee14090e
Dhanraj Prem Kotian <dhanrajkotian3@gmail.com>
2018-05-04 01:15:44 +0530

    Add files via upload

---
+++ DP3.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<cstring>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	string str1,str2;
+	
+	cout<<"Enter first string: ";
+	getline(cin,str1);
+	cout<<"Enter second string: ";
+	getline(cin,str2);
+	
+	int len1,len2;
+	
+	len1 = str1.length();
+	len2 = str2.length();
+	
+	int Subs[len1+1][len2+1];
+	
+	for(int i=0;i<len1+1;i++)
+		Subs[i][0] = 0;
+	for(int i=0;i<len2+1;i++)
+		Subs[0][i] = 0;
+	
+	for(int i=1;i<len1+1;i++)
+	{
+		for(int j=1;j<len2+1;j++)
+		{
+			if(str1[i-1] == str2[j-1])
+				Subs[i][j] = Subs[i-1][j-1] + 1;
+			else
+				Subs[i][j] = max(Subs[i-1][j],Subs[i][j-1]);
+		}
+	}
+	
+	for(int i=0;i<len1+1;i++)
+	{
+		for(int j=0;j<len2+1;j++)
+			cout<<Subs[i][j]<<" ";
+		cout<<"\n";
+	}
+
+	int h_no=Subs[len1][len2],h_row=len1,h_col=len2;
+	
+	char str3[h_no];
+	
+	str3[h_no] = '\0';
+	
+	
+	while(h_no>0)
+	{
+		if(Subs[h_row][h_col] == Subs[h_row-1][h_col-1]+1)
+		{	
+			str3[h_no-1] = str1[h_row-1];
+			h_no--;
+			h_row--;
+			h_col--;
+		}
+		else
+		{
+			if(Subs[h_row-1][h_col]>Subs[h_row][h_col-1])
+				h_row--;
+			else if(Subs[h_row-1][h_col] == Subs[h_row][h_col-1])
+				h_row--;
+			else
+				h_col--;
+		}
+		cout<<h_no<<" "<<h_row<<" "<<h_col<<"\n";
+		fflush(stdout);
+	}
+	
+	cout<<"Longest Subsequence is "<<str3;
+	
+	return 0;
+}
